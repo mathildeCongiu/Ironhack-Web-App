93c7371eaf1aed7d0e0138ee3ea62a629703adc6
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-10-11 12:13:14 +0200

    Fetch to Heroku and fetch to Wiki working. Next step: retrieve information to HTML and add some try catch

--- scripts/dataRetrieving.js
+++ scripts/dataRetrieving.js
@@ -1,27 +1,58 @@
-const gmObjUrl = 'https://chessgmplayers.herokuapp.com/object';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+// Link to our JSON file on Heroku Server
+const gmObjUrl = 'https://chessgmplayers.herokuapp.com/objects';
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
+// Variables for selectors
 const gmList = document.querySelector('#chessGM');
-const btn = document.querySelector("#date");
+const btnCheckIt = document.querySelector("#checkIt");
+let userBirthdayValue = document.querySelector("#start").value;
 
+console.log(userBirthdayValue)
+//Functions
 async function getGMlist(){
     const gmResponse = await fetch(gmObjUrl);
     const gmJSON = await gmResponse.json();
-
-    const profiles = gmJSON.map( async person => {
-    let name = await person.Name.split(", ")[1] + "_" + person.Name.split(", ")[0];
-    const profileResponse = await fetch(wikiUrl + name);
-    const profileJSON = await profileResponse.json();
     
-    
-
-    return profileJSON// Check what the meaning of this
-    });
-    // habria que hacer un segundo intento agrenado (chess_player) al final 
-    // habría que coger los casos de error y agregar una frase de tipo: `This grand-master is from ${person.country} and have been titled in ${person.year} the International Grand Master title. 
-
-    return profiles
+    return gmJSON
 }
+
+console.log(getGMlist)
+
+async function checkBirthdayMatch(userBirthday) {
+    const gmList = await getGMlist();
+
+    let resultMatchArr = [];
+    
+    for (let i= 0; i< gmList.length; i++) {
+        if (await userBirthday === gmList[i].born) {
+            resultMatchArr.push(gmList[i]);
+        }
+    }
+    console.log(resultMatchArr)
+    return resultMatchArr
+}
+
+checkBirthdayMatch(userBirthdayValue)
+
+async function findWikiInfo(page) {
+    let resultMatchArr = await checkBirthdayMatch(userBirthdayValue);
+    for (let i= 0; i< resultMatchArr.length; i++) {
+        let wikiDestructuration = await resultMatchArr[i].wikiPage.split("https://en.wikipedia.org/wiki/")
+        let redirection = await wikiDestructuration[1];
+        // let name = await person.Name.split(", ")[1] + "_" + person.Name.split(", ")[0];
+         let wikiPage = await fetch (wikiUrl + redirection);
+         let response = await wikiPage.json();
+         let thumbnail = await response.thumbnail.source;
+         let extract = await response.extract;
+
+         // Create a new Array to store this new info ?
+         // Do a catch try if there is no answer to fecth to wikipedia
+         // retrieve info in function generate HTML
+        }
+    
+}
+
+findWikiInfo(userBirthdayValue)
 
 function generateHTML(list) {
     list.map(data => {
@@ -49,18 +80,11 @@ function generateHTML(list) {
 }
 
 
-
-btn.addEventListener('click', async (event) => {
+btnCheckIt.addEventListener('click', async (event) => {
     event.target.textContent = "Loading...";
     const gmList = await getGMlist();
     await Promise.all(gmList)
     .then(values => generateHTML(values));
     event.target.remove()
 });
-
-
-// window,addEventListener("load", getGMlist)
-
-// Calendar
-
 
