7cd97f5325cac402f42637e1316061eddebc37cb
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-10-12 20:34:29 +0200

    Functions seem almost ok. A problem with thumbnail fro, line 82 to 92 i don't understand why it gives me error.

--- scripts/dataRetrieving.js
+++ scripts/dataRetrieving.js
@@ -7,124 +7,180 @@ const gmList = document.querySelector('#chessGM');
 const btnCheckIt = document.querySelector("#checkIt");
 let userBirthdayValue = document.querySelector("#start").value;
 
-console.log(userBirthdayValue)
 //Functions
 async function getGMlist(){
     const gmResponse = await fetch(gmObjUrl);
     const gmJSON = await gmResponse.json();
-    console.log(gmJSON, "Response GM")
-    return gmJSON
+    // console.log(gmJSON, "Response GM")
+    return gmJSON;
 }
-
-getGMlist();
 
 async function checkBirthdayMatch(userBirthday) {
     const gmList = await getGMlist();
 
-    let resultMatchArr = [];
+    let arrAllResults = []
+
+    let perfectResultMatchArr = [];
+    let birthdayMatchArr = [];
+    let yearMatchArr = [];
     
+    
+    let userDay =  userBirthday.toString().split("-")[2];
+    let userMonth =  userBirthday.toString().split("-")[1];
+    let userYear =  userBirthday.toString().split("-")[0];
+    let userDayMonth =   userDay + "-" + userMonth;
+
+
     for (let i= 0; i< gmList.length; i++) {
-        if (await userBirthday === gmList[i].born) {
-            resultMatchArr.push(gmList[i]);
+      let gmDay =  gmList[i].born.toString().split("-")[2];
+      let gmMonth =  gmList[i].born.toString().split("-")[1];
+      let gmYear =  gmList[i].born.toString().split("-")[0];
+      let gmDayMonth =   gmDay + "-" + gmMonth;
+      // console.log("aquí está la info", gmDay, gmMonth, gmYear, gmDayMonth);
+        if ( userBirthday === gmList[i].born) {
+            perfectResultMatchArr.push(gmList[i]);
+            
+            
         }
+        else if ( userDayMonth ===  gmDayMonth) {
+          birthdayMatchArr.push(gmList[i]);
+        }
+        else if ( userYear ===  gmYear) {
+          yearMatchArr.push(gmList[i]);
+        }      
     }
-    console.log(resultMatchArr)
-    return resultMatchArr
+    arrAllResults.push(perfectResultMatchArr);
+    arrAllResults.push(birthdayMatchArr);
+    arrAllResults.push(yearMatchArr);
+    console.log(arrAllResults);
+
+    return arrAllResults;
 }
 
-checkBirthdayMatch(userBirthdayValue)
+checkBirthdayMatch(userBirthdayValue);
 
 async function findWikiInfo() {
-    let wikiProfiles = []
-    let resultMatchArr = await checkBirthdayMatch(userBirthdayValue);
-    for (let i= 0; i< resultMatchArr.length; i++) {
-        let wikiDestructuration = await resultMatchArr[i].wikiPage.split("https://en.wikipedia.org/wiki/")
+    let wikiProfiles = [];
+
+    let perfectMatch = [];
+    let birthdayMatch = [];
+    let yearMatch = []
+    let arrAllResults = await checkBirthdayMatch(userBirthdayValue);
+
+    for (let i = 0; i < arrAllResults.length; i++) {
+      if (arrAllResults[i].length > 0) {
+        arrAllResults[i].forEach( async data => {
+         if (data.wikiPage && data.wikiPage.includes("https://en.wikipedia.org/wiki/")) {
+          // console.log(data.wikiPage)
+        let wikiDestructuration = await data.wikiPage.split("https://en.wikipedia.org/wiki/");
         let redirection = await wikiDestructuration[1];
+      
+          let wikiPage = await fetch (wikiUrl + redirection);
+          // console.log(wikiPage);
+          let response = await wikiPage.json();
+
+          let thumbnail
+          if (await response.thumbnail) {
+             thumbnail = response.thumbnail
+          }
+          // else if (await response.thumbnail.source) {
+          //   thumbnail = response.thumbnail.source
+          // }
+
+          else {
+            thumbnail = "content/img/king.jpg"
+          }
+
+          // let thumbnail = await response.thumbnail.source;
+          let extract = await response.extract;
+          // console.log(extract);
+          let name = await response.title;
+         wikiProfile= {
+             name: name,
+             thumbnail: thumbnail,
+             extract: extract
+         };
+
+        //  console.log(wikiProfile, "AHORA ES LA SIGUIENTE COMPROBACION")
+         
+         if (i=== 0) {
+           perfectMatch.push(wikiProfile);
+         }
+
+         else if (i === 1 ) {
+          birthdayMatch.push(wikiProfile);
+         }
+
+         else if (i === 2) {
+          yearMatch.push(wikiProfile);
+         }
+         
+        }
+        else {
+          // console.log(`${data.name} is a player from ${data.mostRecentFed} who got the Grand Master title in ${data.yearTitle}`);
+          if (i=== 0) {
+            perfectMatch.push(data);
+          }
+ 
+          else if (i === 1 ) {
+           birthdayMatch.push(data);
+          }
+ 
+          else if (i === 2) {
+           yearMatch.push(data);
+          }
+        }
         
-         let wikiPage = await fetch (wikiUrl + redirection);
-         console.log(wikiPage);
-         let response = await wikiPage.json();
-         let thumbnail = await response.thumbnail.source;
-         let extract = await response.extract;
-         console.log(extract);
-         let name = await response.title;
-        wikiProfile= {
-            name: name,
-            thumbnail: thumbnail,
-            extract: extract
-        }
-
-        wikiProfiles.push(wikiProfile);
-        console.log(wikiProfiles);
-
-         // Do a catch try if there is no answer to fecth to wikipedia
-         // retrieve info in function generate HTML
-        }
+        })
+      }
+      else {console.log("There is no player born the exact same day as you")
+    };
     
 
+  }
+  
+  wikiProfiles.push(perfectMatch, birthdayMatch, yearMatch)
+
+     
+        // console.log(wikiProfiles)
         return wikiProfiles
-}
+    }
+
 
 findWikiInfo();
 let infoPlayers = findWikiInfo();
 
-console.log(infoPlayers);
-
-function generateHTML2(){
-    let resultContainer = document.querySelector(".result-container");
-    let newCard = document.createElement("div");
-    resultContainer.appendChild(newCard);
-
-
-        newCard.innerHTML = `<div class="card">
-    <div class="thumbnail-container">
-      <img class="thumbnail" src="" alt="random-GM" />
-    </div>
-    <div class="info-gm">
-      <div class="basic-info">
-        <h3 class="gm-name"> Hulk </h3>
-      </div>
-      <div class="bio">
-        <p>Impressive character that never has been grand master</p>
-      </div>
-    </div>
-  </div>`
-}
-
-
  async function generateHTML(gmArray) {
    let result = await gmArray
-    result.map(data => { 
-      console.log(data,'data probando')
-        let resultContainer = document.querySelector(".result-container")
-        let newCard = document.createElement("div");
-        resultContainer.appendChild(newCard);
-  
-        const thumbnail = data.thumbnail ? data.thumbnail : data.thumbnail.source;
-        
-            newCard.innerHTML = `<div class="card">
-        <div class="thumbnail-container">
-          <img class="thumbnail" src=${thumbnail} alt="random-GM" />
-        </div>
-        <div class="info-gm">
-          <div class="basic-info">
-            <h3 class="gm-name">${data.name}</h3>
-          </div>
-          <div class="bio">
-            <p>${data.extract}</p>
-          </div>
-        </div>
-      </div>`
-      console.log(data.extract);
-})
-    //   resultContainer.appendChild(newCard)
 
-    //     // let tagForInfo = document.querySelector(".home");
-    //     // let tagCreated = document.createElement("p");
-    //     // let appended = tagForInfo.appendChild(tagCreated);
-    //     // appended.innerHTML = data.title;
-    // })
-}
+   for (let i = 0; i < result.length; i++) {
+     result[i].map(data => { 
+        console.log(data,'data probando')
+          let resultContainer = document.querySelector(".result-container")
+          let newCard = document.createElement("div");
+          resultContainer.appendChild(newCard);
+
+
+    
+          // const thumbnail = data.thumbnail ? data.thumbnail : data.thumbnail.source;
+          
+              newCard.innerHTML = `<div class="card">
+          <div class="thumbnail-container">
+            <img class="thumbnail" src=${thumbnail} alt="random-GM" />
+          </div>
+          <div class="info-gm">
+            <div class="basic-info">
+              <h3 class="gm-name">${data.name}</h3>
+            </div>
+            <div class="bio">
+              <p>${data.extract}</p>
+            </div>
+          </div>
+        </div>`
+  })
+     }
+   }
+
 
 generateHTML(infoPlayers)
 
@@ -135,45 +191,3 @@ btnCheckIt.addEventListener('click', (event) => {
     .then(values => generateHTML(values));
     event.target.remove()
 });
-
-
-
-
-//Functions for other possible matches (with month or year)
-
-// async function checkBirthYearMatch(userBirthday) {
-//     const gmList = await getGMlist();
-
-//     let resultMatchArrYear = [];
-    
-//     for (let i= 0; i< gmList.length; i++) {
-//         let userBirthYear = await userBirthday.split("-")[0];
-//         console.log(userBirthday)
-//         if (await userBirthYear === gmList[i].born.split("-")[0]) {
-//             resultMatchArrYear.push(gmList[i]);
-//         }
-            
-//         }
-    
-//     console.log(resultMatchArrYear)
-//     return resultMatchArrYear
-// }
-
-
-
-// async function checkBirthMonthYearMatch(userBirthday) {
-//     const gmList = await getGMlist();
-
-//     let resultMatchArrMonthYear = [];
-    
-//     for (let i= 0; i< gmList.length; i++) {
-//         let userBirthMonthYear = await userBirthday.split("-")[0] + "-" + userBirthday.split("-")[1];
-//         if (await userBirthMonthYear === gmList[i].born.split("-")[0]) {
-//             resultMatchMonthYear.push(gmList[i]);
-//         }
-            
-//         }
-    
-//     console.log(resultMatchArrMonthYear)
-//     return resultMatchArrMonthYear
-// }
